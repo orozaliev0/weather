e63fad56084f33cef39ae14afab7d5d0e609418e
orozaliev0 <orozaliev.pp@gmail.com>
2023-03-13 17:49:59 +0600

    first commit

---
+++ src/Weather_app/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import TopNav from "./TopNav"
+import Application from "./Application"
+import "./style.css"
+
+
+const Main = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/Weather_app/api/FetchWeather.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FethWeather = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default FethWeather;
